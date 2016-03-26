685050660ab8df26e0d7626f11f295114ad0d2a3
Nikhil Gupta <ng3br@virginia.edu>
2016-03-26 17:56:20 +0000

    Fixed url patterns

--- story/makestory/urls.py
+++ story/makestory/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import patterns, url
 from makestory import views
 
-urlpatterns = [
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'django_project.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
     url(r'^$', views.index, name='input'),
     url(r'process/$', views.process, name='process'),
     url(r'output/$', views.output, name='output'),
     url(r'fail/$', views.fail, name='fail'),
-]
+)
