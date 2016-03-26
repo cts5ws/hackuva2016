4302ee7860feaf8ce0c002f34b5771bb8755ad67
Nikhil Gupta <ng3br@virginia.edu>
2016-03-26 19:28:52 +0000

    removed models, updated views, updated hmtl

--- story/makestory/models.py
+++ story/makestory/models.py
@@ -1,19 +1,6 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.core.files import File
-import os, urllib
+import os
 
 # Create your models here.
-class Image(models.Model):
-    image_file = models.FileField(upload_to='images/')
-    image_url = models.URLField()
-    
-def get_remote_image(self):
-    if self.image_url and not self.image_file:
-        result = urllib.urlretrieve(self.image_url)
-        self.image_file.save(
-            os.path.basename(self.image_url),
-            File(open(result[0]))
-        )
-        self.save()
--- story/makestory/views.py
+++ story/makestory/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.db import models
 from makestory.models import Image
+from clarifai.client import ClarifaiApi
+import json
 
 # Create your views here.
 def index(request):
@@ -9,10 +12,25 @@ def index(request):
 def process(request):
     if request.method == "GET":
         if 'inputURL' in request.GET:
+            # Validation of image url
             imageURL = request.GET['inputURL']
-            image = Image(image_url=imageURL)
-            image.save()
-            return output(request)
+            indexOfDot = rfind(".")
+            extension = imageURL[indexOfDot:]
+            
+            
+            extension3 = imageURL[-3:]
+            extension4 = imageURL[-4:]
+            if extension3 = 'jpgs'
+            
+            clarifai_api = ClarifaiApi() # assumes environment variables are set.
+            result = clarifai_api.tag_images(imageURL)
+            
+            class_list = result['results'][0]['result']['tag']['classes']
+            prob_list = result['results'][0]['result']['tag']['probs']
+            
+            
+            
+            return output(request, {image_output: 'image_output', text_output: 'text_output')
         else:
             return index(request)
     return fail(request)
