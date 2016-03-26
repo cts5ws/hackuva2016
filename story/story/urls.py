6bf7e14a302686baef1d5a8aa30fd7abb1d7004a
Nikhil Gupta <ng3br@virginia.edu>
2016-03-26 12:08:11 -0400

    First commit

---
+++ story/story/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include, url
+import views
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'django_project.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+)
