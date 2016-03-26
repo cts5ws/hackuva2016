4d07cee3e8120b653f24970900ae6974cee4b794
Nikhil Gupta <ng3br@virginia.edu>
2016-03-26 18:51:36 +0000

    imported RedirectView

--- story/story/urls.py
+++ story/story/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'django_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     
-    url(r'^$', RedirectView.as_view(pattern_name="makestory")),
+    url(r'^$', RedirectView.as_view(url='/makestory')),
     url(r'^makestory/', include('makestory.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
