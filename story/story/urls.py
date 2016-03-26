028bd23da73aa51d84136c64b9082ccb99f7fae4
Nikhil Gupta <ng3br@virginia.edu>
2016-03-26 17:50:05 +0000

    makestory.views added to urls.py

--- story/story/urls.py
+++ story/story/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from . import makestory.views
 
 admin.autodiscover()
 
@@ -9,5 +10,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', include('makestory.urls')),
+    url(r'^$/', include('makestory.urls')),
 )
