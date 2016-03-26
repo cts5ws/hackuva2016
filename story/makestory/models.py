325610a33630192ae6eeedf58d0004e7cc8b7594
Nikhil Gupta <ng3br@virginia.edu>
2016-03-26 17:28:08 +0000

    Removing pillow dependencies for testing purposes

--- story/makestory/models.py
+++ story/makestory/models.py
@@ -2,18 +2,18 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.core.files import File
-import os
+import os, urllib
 
 # Create your models here.
-class Image(models.Model):
-    image_file = models.ImageField(upload_to='images')
-    image_url = models.URLField()
+# class Image(models.Model):
+#     image_file = models.ImageField(upload_to='images')
+#     image_url = models.URLField()
     
-def get_remote_image(self):
-    if self.image_url and not self.image_file:
-        result = urllib.retrieve(self.image_url)
-        self.image_file.save(
-            os.path.basename(self.image_url),
-            File(open(result[0]))
-        )
-        self.save()
+# def get_remote_image(self):
+#     if self.image_url and not self.image_file:
+#         result = urllib.urlretrieve(self.image_url)
+#         self.image_file.save(
+#             os.path.basename(self.image_url),
+#             File(open(result[0]))
+#         )
+#         self.save()
