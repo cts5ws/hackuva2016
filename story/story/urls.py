4f1ec4dc8c63ef2913496a8797895e4e60971f32
Nikhil Gupta <ng3br@virginia.edu>
2016-03-26 18:01:30 +0000

    a

--- story/story/urls.py
+++ story/story/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'django_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$/', include('makestory.urls'),
+    url(r'^$/', include('makestory.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^makestory/', include('makestory.urls')),
 )
