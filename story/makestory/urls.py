e6b66bc8398eb0a8999d21679f657fc5bbe37f78
Nikhil Gupta <ng3br@virginia.edu>
2016-03-26 17:57:16 +0000

    Fixed order of urls

--- story/makestory/urls.py
+++ story/makestory/urls.py
@@ -6,8 +6,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'django_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^process/$', views.process, name='process'),
+    url(r'^output/$', views.output, name='output'),
+    url(r'^fail/$', views.fail, name='fail'),
     url(r'^$', views.index, name='input'),
-    url(r'process/$', views.process, name='process'),
-    url(r'output/$', views.output, name='output'),
-    url(r'fail/$', views.fail, name='fail'),
 )
