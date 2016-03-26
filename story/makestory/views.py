8c76a2a76497bc5eb41b2ff0ce4131af6f17e980
Nikhil Gupta <ng3br@virginia.edu>
2016-03-26 19:38:58 +0000

    image validation, some clarifai

--- story/makestory/views.py
+++ story/makestory/views.py
@@ -10,27 +10,29 @@ def index(request):
     return render(request, 'makestory/index.html')
     
 def process(request):
+    # Validation of form
     if request.method == "GET":
+        # Validation of request
         if 'inputURL' in request.GET:
             # Validation of image url
             imageURL = request.GET['inputURL']
             indexOfDot = rfind(".")
+            indexOfDot += 1
             extension = imageURL[indexOfDot:]
-            
-            
-            extension3 = imageURL[-3:]
-            extension4 = imageURL[-4:]
-            if extension3 = 'jpgs'
-            
+            if extension != 'jpg' and extension != 'jpeg' and extension != 'png':
+                return fail(request) # not a valid image (jpg, jpeg, png)
+                
             clarifai_api = ClarifaiApi() # assumes environment variables are set.
             result = clarifai_api.tag_images(imageURL)
             
             class_list = result['results'][0]['result']['tag']['classes']
             prob_list = result['results'][0]['result']['tag']['probs']
             
+            #currently just the list of matched words
+            text_output = class_list.__str__()
             
-            
-            return output(request, {image_output: 'image_output', text_output: 'text_output')
+            image_output = imageURL
+            return render(request, 'makestory/output.html', {image_output:'image_output', text_output:'text_output'})
         else:
             return index(request)
     return fail(request)
