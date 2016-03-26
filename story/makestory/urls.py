e8fd875c7a0435aa2ac925b3cd5cc34e5e989aab
Nikhil Gupta <ng3br@virginia.edu>
2016-03-26 17:59:49 +0000

    trying different urls

--- story/makestory/urls.py
+++ story/makestory/urls.py
@@ -1,13 +1,10 @@
 from django.conf.urls import patterns, url
 from makestory import views
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'django_project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^process/$', views.process, name='process'),
-    url(r'^output/$', views.output, name='output'),
-    url(r'^fail/$', views.fail, name='fail'),
+urlpatterns = [
     url(r'^$', views.index, name='input'),
-)
+    url(r'^process/', views.process, name='process'),
+    url(r'^output/', views.output, name='output'),
+    url(r'^fail/', views.fail, name='fail'),
+    
+]
