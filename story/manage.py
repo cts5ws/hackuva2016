b5795f7e393c7d1b77703cb6389fe8e979e26740
Nikhil Gupta <ng3br@virginia.edu>
2016-03-27 14:17:57 +0000

    manage change

--- story/manage.py
+++ story/manage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
 import sys
+#from models import Bigram
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "story.settings")
@@ -8,3 +9,17 @@ if __name__ == "__main__":
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
+
+def create_db():
+    BASE = os.path.dirname(os.path.abspath(__file__))
+    f = open(os.path.join(BASE, 'makestory/output.txt'), 'r')
+    instances = [
+        Bigram(
+            first_word = line.split()[0],
+            next_word = line.split()[1],
+            frequency = line.split()[2],
+        )
+        for line in f
+    ]
+    Bigram.objects.bulk_create(instances)
+    f.close()
