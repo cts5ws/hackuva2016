4c4dab68a845dfdfc408c19a8e62dfd20581ebbd
Nikhil Gupta <ng3br@virginia.edu>
2016-03-27 07:23:18 +0000

    added set up code

--- story/makestory/views.py
+++ story/makestory/views.py
@@ -12,9 +12,24 @@ from models import Bigram
 import sys
 
 sys.setrecursionlimit(10000)
+bool first_access = True
 
 # Create your views here.
 def index(request):
+    if first_access:
+        f = open('../static/rec/w2_.txt', 'r')
+        models = []
+        for line in f:
+            sp = line.split() # sp[0] = freq, sp[1] = first_word, sp[2] = second_word
+            freq = int(sp[0])
+            first = sp[1]
+            second = sp[2]
+            bigram = Bigram(first_word=first, next_word=second, frequency=freq)
+            models.append(bigram)
+        for model in models:
+            model.save()
+        f.close()
+        first_access = False
     return render(request, 'makestory/index.html')
     
 def output(request):
@@ -178,7 +193,19 @@ def get_validity(sentences):
         validity[sentence] = 0
         for word in words:
             bigrams = Bigram.objects.filter(
-                first_word=prev_word,
+                first_word=prev_worf = open('../static/rec/w2_.txt', 'r')
+    
+        models = []
+        for line in f:
+            sp = line.split() # sp[0] = freq, sp[1] = first_word, sp[2] = second_word
+            freq = int(sp[0])
+            first = sp[1]
+            second = sp[2]
+            bigram = Bigram(first_word=first, next_word=second, frequency=freq)
+            models.append(bigram)
+        for model in models:
+            model.save()
+        f.close()d,
                 next_word=word
             )
             
