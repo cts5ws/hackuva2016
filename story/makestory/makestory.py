27d50a9782bcd3afd27e344dc591751ea375fef2
Nikhil Gupta <ng3br@virginia.edu>
2016-03-27 06:46:16 +0000

    app config

--- story/makestory/makestory.py
+++ story/makestory/makestory.py
@@ -1,21 +1,20 @@
+from Django.apps import AppConfig
 from models import Bigram
 
-def read_bigrams():
-    f = open('../static/rec/w2_.txt', 'r')
+class makeStoryConfig(AppConfig):
+    name = 'makestory'
+    verbose_name = "Story Time"
+    def read(self):
+        f = open('../static/rec/w2_.txt', 'r')
     
-    models = []
-    for line in f:
-        sp = line.split() # sp[0] = freq, sp[1] = first_word, sp[2] = second_word
-        
-        freq = int(sp[0])
-        first = sp[1]
-        second = sp[2]
-        bigram = Bigram(first_word=first, next_word=second, frequency=freq)
-        
-        models.append(bigram)
-        
-    for model in models:
-        model.save()
-        
-    f.close()
-        
+        models = []
+        for line in f:
+            sp = line.split() # sp[0] = freq, sp[1] = first_word, sp[2] = second_word
+            freq = int(sp[0])
+            first = sp[1]
+            second = sp[2]
+            bigram = Bigram(first_word=first, next_word=second, frequency=freq)
+            models.append(bigram)
+        for model in models:
+            model.save()
+        f.close()
--- story/story/urls.py
+++ story/story/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import RedirectView
-from makestory import read_bigrams
 
 admin.autodiscover()
 
@@ -14,5 +13,3 @@ urlpatterns = patterns('',
     url(r'^makestory/', include('makestory.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
-
-read_bigrams()
