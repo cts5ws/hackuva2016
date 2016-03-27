1dcd9d588c1bf8156789b61e0f456d33eeb84315
Nikhil Gupta <ng3br@virginia.edu>
2016-03-27 04:52:04 +0000

    change import on urls

--- story/makestory/urls.py
+++ story/makestory/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, url
-from . import views
+import views
 
 urlpatterns = [
     url(r'^$', views.index, name='input'),
--- story/makestory/views.py
+++ story/makestory/views.py
@@ -7,7 +7,7 @@ from nltk import pos_tag, CFG
 from nltk.parse.generate import generate
 import json
 from django.contrib import messages
-from . import models.Bigram
+import models.Bigram
 
 import sys
 
