ff988ff8dab3ed1689e34e152b2b160f6778320c
Nikhil Gupta <ng3br@virginia.edu>
2016-03-27 08:06:02 +0000

    Tryna fix this shit still

--- story/makestory/views.py
+++ story/makestory/views.py
@@ -17,7 +17,7 @@ sys.setrecursionlimit(10000)
 def index(request):
     if len(list(Bigram.objects.all())) == 0:
         BASE = os.path.dirname(os.path.abspath(__file__))
-        f = open(os.path.join(BASE, 'w2_.txt'), 'r')
+        f = open(os.path.join(BASE, 'w2_test.txt'), 'r')
         models = []
         for line in f:
             sp = line.split() # sp[0] = freq, sp[1] = first_word, sp[2] = second_word
@@ -25,9 +25,7 @@ def index(request):
             first = sp[1]
             second = sp[2]
             bigram = Bigram(first_word=first, next_word=second, frequency=freq)
-            models.append(bigram)
-        for model in models:
-            model.save()
+            bigram.save()
         f.close()
     return render(request, 'makestory/index.html')
     
